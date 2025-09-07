16ce588c1e0ab6d7df3d34953606f92292f4b260
Pola_rizz <mewtwokirisaki@gmail.com>
2025-09-08 01:18:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function openBurgerNav() {
+    document.querySelector(".hamburger-nav").classList.remove("hidden");
+}
+
+function hButton () {
+    document.querySelector(".hamburger-nav").classList.add("hidden");
+}
